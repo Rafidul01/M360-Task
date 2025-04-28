5eca73176ce643bc89ecf6899c8af50a27fe3e22
Rafidul01 <rafidulhasan555@gmail.com>
2025-04-28 13:37:55 +0600

    React router, vite and TS setup done

---
+++ src/Router/Router.tsx
@@ -0,0 +1,24 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Root from "../Root/Root";
+import App from "../App";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        children: [
+            {
+                path:"/",
+                element: <App/>
+            }
+
+
+        ],
+    },
+]);
+
+export default router;
